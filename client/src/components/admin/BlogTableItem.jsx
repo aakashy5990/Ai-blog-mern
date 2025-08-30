a1d095ad18eaaac1f6fc70bc9a24cbc3bfbc44cb
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2025-08-31 05:08:05 +0530

    Comments section Done

---
+++ client/src/components/admin/BlogTableItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { assets } from '../../assets/assets';
+
+const BlogTableItem = ({ blog, fetchBlogs, index }) => {
+  const { title, createdAt } = blog;
+  const BlogDate = new Date(createdAt);
+
+  return(
+  <>
+    <tr className="border-y border-gray-300">
+      <th className="px-2 py-4">{index}</th>
+      <th className="px-2 py-4">{title}</th>
+      <th className="px-2 py-4 max-sm:hidden">{BlogDate.toDateString()}</th>
+      <th className="px-2 py-4 max-sm:hidden">
+        <p
+          className={`${
+            blog.isPublished ? 'text-green-600' : 'text-orange-700'
+          }`}
+        >
+          {blog.isPublished ? 'Published' : 'UnPublished'}
+        </p>
+      </th>
+      <th className="px-2 py-4 flex text-xs gap-3">
+        <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
+          {blog.isPublished ? 'UnPublish' : 'Publish'}
+        </button>
+        <img
+          src={assets.cross_icon}
+          className="w-8 hover:scale-110 transition-all cursor-pointer"
+          alt=""
+        />
+      </th>
+    </tr>
+  </>
+  )
+};
+
+export default BlogTableItem;
